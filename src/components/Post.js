a10938fb3daad6f9ddcb1e3c23015fefe8ffe926
Rafael Rollo <rafaelrollo92@gmail.com>
2018-10-17 16:13:46 -0300

    final do like

--- src/components/Post.js
+++ src/components/Post.js
@@ -18,9 +18,23 @@ export class Post extends Component {
   }
 
   like = () => {
+    let novaLista = [];
+
+    //novaLista = this.state.foto.likers.concat({login: 'meuUsuario'}) 
+    if (!this.state.foto.likeada) {
+      novaLista = [
+        ...this.state.foto.likers,
+        {login: 'meuUsuario'}
+      ]
+    } else {
+      novaLista = this.state.foto.likers
+          .filter(liker => liker.login !== 'meuUsuario')
+    }
+
     const fotoAtualizada = {
       ...this.state.foto,
-      likeada: !this.state.foto.likeada
+      likeada: !this.state.foto.likeada,
+      likers: novaLista
     }
     //Object.assign({}, this.state.foto, {likeada: !this.state.foto.likeada})
 
@@ -47,6 +61,19 @@ export class Post extends Component {
                 require('../../resources/img/s2-checked.png') : require('../../resources/img/s2.png') }
                 style={styles.botaoDeLike}/>
           </TouchableOpacity>
+
+          {
+            foto.likers.length > 0 && 
+              <Text style={styles.likes}>
+                {foto.likers.length} curtidas
+              </Text>
+          }
+          
+          <Text style={styles.comentario}>
+            <Text>
+              <Text style={styles.titulo}>{foto.loginUsuario}</Text> {foto.comentario}
+            </Text>
+          </Text>
         </View>
       </View>
     )
@@ -79,5 +106,17 @@ const styles = StyleSheet.create({
   botaoDeLike: {
     width: 40,
     height: 40
+  },
+  likes: {
+    marginTop: 5,
+    fontWeight: 'bold'
+  },
+  comentario: {
+    marginTop: 5,
+    flexDirection: 'row'
+  },
+  titulo: {
+    fontWeight: 'bold',
+    marginRight: 5
   }
 })
