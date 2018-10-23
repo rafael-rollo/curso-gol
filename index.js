8d26418e4ae42e0e080c935af8144d9219978564
Rafael Rollo <rafaelrollo92@gmail.com>
2018-10-23 09:53:05 -0300

    Implementando login

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { AppRegistry } from 'react-native';
 import Feed from './src/components/Feed';
+import Login from './src/screens/Login';
 
-AppRegistry.registerComponent('InstaluraMobile', () => Feed);
+AppRegistry.registerComponent('InstaluraMobile', () => Login);
